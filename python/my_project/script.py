64e618196c1840c8a20fe211af6cf08f5bdd5153
aymoon <61629065+aym00n-djrak@users.noreply.github.com>
2023-09-01 16:04:50 +0200

    change

---
+++ python/my_project/script.py
@@ -0,0 +1,15 @@
+import sys
+
+print("Hello world!")
+
+
+print(sys.version)
+print(sys.executable)
+print(sys.path)
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+print(greet("World"))
+print(greet("Corey"))
