000a091df905c691e57e1b6e338c28a2be5bff6a
Olgierd <olgierdkrzyzaniak@gmail.com>
2023-09-11 22:07:17 +0200

    Add crypto files and solve exercises

---
+++ 03 - Crypto/301_T01_A01_ Asym_KeyGen_EnDe/301_T01_A01_ Asym_KeyGen_EnDe/Asym.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""Asymmetric Cryptography -> Key Generation: Tutorial 1
+
+The goal of this tutorial is to learn how to generate and use asymmetric keys.
+Additionally, you will learn how to encrypt and decrypt messages using generated private and public keys.
+In this implementation the message is a string converted to a byte object.
+Both methods encrypt and decrypt accept two args: a message and a key to perform its operation.
+
+Your task is to:
+    * locate the TODOs in this file
+    * complete the missing part from the code 
+    * run the test of this tutorial located in same folder.
+
+To test run 'Asym_t.py' in your command line
+
+Notes:
+    * do not change class structure or method signature to not break unit tests
+    * visit this url for more information on this topic:
+    https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/
+"""
+
+from cryptography.exceptions import *
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
+# TODO 1: Generate first a private key, then a public key. As a result return both values.
+# Make sure you generate the keys in the correct order.
+# Use recommended algorithm values where possible
+# Suggested key size 2048
+def generate_keys():
+    private = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    public = private.public_key()
+    return private, public
+
+
+# TODO 2: Encrypt a passed message using the provided key
+# Suggested algorithm is SHA256()
+def encrypt(message, key):
+    ciphertext = key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None,
+        ),
+    )
+    return ciphertext
+
+
+# TODO 3: Decrypt a passed message using the provided key
+# Make sure using the same recommended algorithm values for encryption and decryption
+# Suggested algorithm is SHA256().
+def decrypt(ciphertext, key):
+    try:
+        plaintext = key.decrypt(
+            ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None,
+            ),
+        )
+        return plaintext
+    except:
+        return None
---
+++ 03 - Crypto/302_T02_A02_ Asym_KeySaveLoad_1/302_T02_A02_ Asym_KeySaveLoad_1/Asym.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+"""Asymmetric Cryptography -> Key Management: Tutorial 2
+
+The goal of this tutorial is to learn how to load and store asymmetric keys on the disk.
+In previous tutorial you have learned how to generate keys and use them directly to encrypt/decrypt a message, 
+but in many cases generated keys are stored on and loaded from a disk when they are needed for a specific application.
+As a part of the loading and saving process keys are de-/serialized. 
+It is optionally possible to encrypted before saving using a password. In our example no passwords are needed. 
+Stored keys in the file system are encoded using PEM encapsulation format. 
+This format has BEGIN {format} and END {format} markers to separate keys. 
+Stored keys are located in a subdirectory called *storage*
+
+Your task is to:
+    * locate the TODOs in this file
+    * complete the missing part from the code 
+    * run the test of this tutorial located in same folder.
+
+To test run 'Asym_t.py' in your command line
+
+Notes:
+    * do not change class structure or method signature to not break unit tests
+    * visit these urls for more information on this topic:
+    https://cryptography.io/en/latest/hazmat/primitives/asymmetric/serialization/
+    https://docs.python.org/3/library/pickle.html
+"""
+from tkinter.messagebox import NO
+from cryptography.exceptions import *
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
+import pickle
+
+
+def generate_keys():
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+
+def encrypt(message, key):
+    try:
+        ciphertext = key.encrypt(
+            message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None,
+            ),
+        )
+        return ciphertext
+    except:
+        return False
+
+
+def decrypt(ciphertext, key):
+    try:
+        plaintext = key.decrypt(
+            ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None,
+            ),
+        )
+        return plaintext
+    except:
+        return False
+
+
+# TODO 1: Store the list of keys into a given file.
+# Make sure of proper PEM encoding before serialization
+def save_keys(keys_file_name, keys_list):
+    serialized_keys = []
+    for key in keys_list:
+        key_name, private_key, public_key = key
+
+        private_serialized = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+        publice_serialized = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        )
+
+        serialized_keys.append((key_name, private_serialized, publice_serialized))
+
+    with open(keys_file_name, "wb") as savefile:
+        pickle.dump(serialized_keys, savefile)
+
+    return serialized_keys
+
+
+# TODO2: Load all private and public keys from a given file and return those keys as a list
+# Make sure of proper PEM decoding when deserializing
+def load_keys(keys_file_name):
+    with open(keys_file_name, "rb") as key_file:
+        deserialized_keys = []
+        serialized_keys_list = pickle.load(key_file)
+        for serialized_key in serialized_keys_list:
+            key_name, private_key_ser, public_key_ser = serialized_key
+            private_key = serialization.load_pem_private_key(
+                private_key_ser, password=None
+            )
+            public_key = serialization.load_pem_public_key(public_key_ser)
+            deserialized_keys.append((key_name, private_key, public_key))
+    return deserialized_keys
