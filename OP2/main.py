4a7f930045f44a105de5877101604bffc6b8f17e
aymoon <61629065+aym00n-djrak@users.noreply.github.com>
2023-10-03 13:52:56 +0200

    rm assignment

--- OP2/main.py
+++
@@ -1,9 +0,0 @@
-from cryptography.hazmat.primitives.asymmetric import rsa
-
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-)
-
-print(private_key.public_key().public_numbers().n)
-print("Hello World!")
